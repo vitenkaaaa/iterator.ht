e8a9ff7ab232f6e9a4b305e5f94a92660adb92e8
vitenkaaaa <puzikovv86@gmail.com>
2025-06-04 10:21:28 +0300

    Add files via upload

--- iterator/src/Randoms.java
+++ iterator/src/Randoms.java
@@ -1,23 +1,49 @@
-import java.util.Iterator;
-import java.util.Random;
+import java.util.*;
 
 public class Randoms implements Iterable<Integer> {
     protected Random random;
+    private List list = new ArrayList();
 
-    public  Randoms(int min, int max) {
+    public Randoms(int min, int max) {
         Random random = new Random();
+        int i = 0;
+        int numd = 0;
         int rand = random.nextInt(max + 1);
-        if (rand < min){
+        if (rand < min) {
             rand = min;
         }
-
-
+//        while (i < 10) {
+//            numd = random.nextInt();
+//            list.add(numd);
+//            i++;
+//        }
     }
-
 
     @Override
     public Iterator<Integer> iterator() {
-        return null;
+        return new Iterator<>() {
+            int next = 0;
+
+            @Override
+            public boolean hasNext() {
+                if (next < list.size()) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+
+            @Override
+            public Integer next() {
+                if (next < list.size()) {
+                    int number = (int) list.get(next);
+                    next++;
+                    return number;
+                }
+                return 0;
+            }
+        };
+
     }
 }
 
